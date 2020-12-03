e773f0f53fba694371b1c7ec42d4bfb5d44ee88b
faustin0 <dinatalefausto@gmail.com>
2020-12-03 17:39:53 +0100

    first tests and implementation of bus client

--- src/main/scala/dev/faustin0/bus/bot/BusInfoClient.scala
+++ src/main/scala/dev/faustin0/bus/bot/BusInfoClient.scala
@@ -1,15 +1,87 @@
 package dev.faustin0.bus.bot
 
 import cats.Applicative
+import cats.effect.{ Blocker, ConcurrentEffect, ContextShift, IO, Resource, Sync }
 import dev.faustin0.bus.bot.models.BusInfoResponse
+import io.circe.generic.auto._
+import org.http4s.Method.GET
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.middleware.{ Logger => ClientLogger }
+import org.http4s.client.{ Client, JavaNetClientBuilder }
+import org.http4s.headers.{ `Content-Type`, Accept }
+import org.http4s.{ Headers, MediaType, Request, Uri }
 
-trait BusInfoClient[F[_]] {
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+
+trait BusInfoDSL[F[_]] {
   def getNextBuses: F[List[BusInfoResponse]]
   def getBusStopInfo: F[String]
 }
 
-class InMemoryBusInfoClient[F[_]: Applicative] extends BusInfoClient[F] {
+class InMemoryBusInfoClient[F[_]: Applicative] extends BusInfoDSL[F] {
   override def getNextBuses: F[List[BusInfoResponse]] = Applicative[F].pure(Nil)
 
   override def getBusStopInfo: F[String] = Applicative[F].pure("")
+}
+
+class Http4sBusInfoClient[F[_]: Sync](private val client: Client[F], uri: Uri) extends BusInfoDSL[F] {
+
+  override def getNextBuses: F[List[BusInfoResponse]] = {
+    val req = Request[F](
+      method = GET,
+      uri = (uri / "bus-stops" / "303")
+        .withQueryParams(
+          Map(
+            "bus" -> "28"
+          )
+        ),
+      headers = Headers.of(
+        Accept(MediaType.application.json),
+        `Content-Type`(MediaType.application.json)
+      )
+    )
+    Sync[F].suspend(client.expect[List[BusInfoResponse]](req))
+  }
+
+  override def getBusStopInfo: F[String] = {
+    val req = Request[F](
+      method = GET,
+      uri = uri.withPath("/health"),
+      headers = Headers.of(
+        Accept(MediaType.application.json)
+      )
+    )
+
+    Sync[F].suspend(client.expect[String](req))
+  }
+
+}
+
+object Http4sBusInfoClient {
+
+  def apply(httpClient: Client[IO], uri: Uri): Http4sBusInfoClient[IO] = new Http4sBusInfoClient(httpClient, uri)
+
+  def makeWithResource(
+    host: String,
+    executionContext: ExecutionContext
+  )(implicit ce: ConcurrentEffect[IO]): Resource[IO, Http4sBusInfoClient[IO]] =
+    BlazeClientBuilder[IO](executionContext)
+      .withConnectTimeout(7 seconds)
+      .withRequestTimeout(7 seconds)
+      .resource
+      .map(client => ClientLogger(logHeaders = false, logBody = true)(client))
+      .map(client => new Http4sBusInfoClient(client, Uri.unsafeFromString(host)))
+
+  def make(
+    host: String,
+    executionContext: ExecutionContext
+  )(implicit ce: ContextShift[IO]): Http4sBusInfoClient[IO] = {
+    val blocker      = Blocker.liftExecutionContext(executionContext)
+    val httpClient   = JavaNetClientBuilder[IO](blocker).create
+    val loggedClient = ClientLogger(logHeaders = false, logBody = true)(httpClient)
+
+    new Http4sBusInfoClient(loggedClient, Uri.unsafeFromString(host))
+  }
 }
--- src/test/scala/dev/faustin0/bus/bot/BusInfoClientTest.scala
+++ src/test/scala/dev/faustin0/bus/bot/BusInfoClientTest.scala
@@ -1,24 +1,74 @@
 package dev.faustin0.bus.bot
 
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.{ Blocker, IO, Resource }
+import com.dimafeng.testcontainers.{ ForAllTestContainer, MockServerContainer }
+import io.circe.literal.JsonStringContext
+import org.http4s.Uri
+import org.http4s.client.{ Client, JavaNetClientBuilder }
+import org.mockserver.client.MockServerClient
+import org.mockserver.model.HttpRequest.request
+import org.mockserver.model.HttpResponse.response
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.testcontainers.containers.wait.strategy.Wait
+
 import java.util.concurrent.Executors
 
-import cats.effect.testing.scalatest.AsyncIOSpec
-import cats.effect.{ Blocker, IO }
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-import org.http4s.client.{ Client, JavaNetClientBuilder }
-import org.scalatest.funsuite.AsyncFunSuite
+class BusInfoClientTest extends AsyncFreeSpec with ForAllTestContainer with AsyncIOSpec with Matchers {
 
-class BusInfoClientTest extends AsyncFunSuite with AsyncIOSpec {
-
-  val blockingPool           = Executors.newFixedThreadPool(5)
+  val blockingPool           = Executors.newFixedThreadPool(4)
   val blocker                = Blocker.liftExecutorService(blockingPool)
   val httpClient: Client[IO] = JavaNetClientBuilder[IO](blocker).create
-  implicit val logger        = Slf4jLogger.getLogger[IO]
 
-  test("should retrieve the next buses for a given stop") {
-
-//    val sut = new BusInfoClient(httpClient)
-
-    assert(true)
+  override val container: MockServerContainer = MockServerContainer("5.11.2").configure { c =>
+    c.withLogConsumer(c => println(c.getUtf8String))
+      .waitingFor(Wait.forLogMessage(".*started on port:.*", 1))
   }
+
+  lazy val mockServerClient: Resource[IO, MockServerClient] = Resource
+    .fromAutoCloseable(
+      IO(new MockServerClient(container.host, container.container.getServerPort))
+    )
+
+  "should retrieve the next buses for a given stop" in {
+
+    mockServerClient.use { mock =>
+      val registerExpectation = IO(
+        mock
+          .when(
+            request()
+              .withPath("/bus-stops/303")
+              .withMethod("GET")
+              .withQueryStringParameter("bus", "28")
+          )
+          .respond(
+            response().withBody(
+              json"""
+                  [
+                    {
+                      "bus": "28",
+                      "satellite": false,
+                      "hour": "23:00",
+                      "busInfo": ""
+                    },
+                    {
+                      "bus": "28",
+                      "satellite": false,
+                      "hour": "23:20",
+                      "busInfo": ""
+                    }
+                  ]
+                """.noSpaces
+            )
+          )
+      )
+      for {
+        _        <- registerExpectation
+        sut      <- IO(Http4sBusInfoClient(httpClient, Uri.unsafeFromString(container.endpoint)))
+        response <- sut.getNextBuses
+      } yield response
+    }.asserting(responses => assert(responses.nonEmpty))
+  }
+
 }
