94f079abfdfa76c1f211d2f54e89b91a4707b67d
Fausto Di Natale <dinatalefausto@gmail.com>
2021-05-16 06:19:31 +0000

    Update cats-effect, cats-effect-laws to 2.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 //  val http4sVersion              = "0.21.3"
   val http4sVersion              = "0.21.22"
   val testcontainersScalaVersion = "0.39.3"
-  val catsVersion                = "2.5.0"
+  val catsVersion                = "2.5.1"
   val kindProjectorV             = "0.11.3"
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.13.0"
