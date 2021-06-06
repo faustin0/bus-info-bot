eb03c6bd6215d190c92230d82b46f6f4257c948b
Fausto Di Natale <dinatalefausto@gmail.com>
2021-06-06 06:46:16 +0000

    Update kind-projector to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val catsVersion                = "2.5.1"
   val http4sVersion              = "0.21.24"
   val testcontainersScalaVersion = "0.39.5"
-  val kindProjectorV             = "0.11.3"
+  val kindProjectorV             = "0.13.0"
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.14.1"
   val log4catsVersion            = "1.1.1"
