43b5fc26fb4d8fb5967952230f127a34aa87525e
Fausto Di Natale <dinatalefausto@gmail.com>
2021-08-01 06:16:53 +0000

    Update dynamodb to 2.17.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.14.1"
   val log4catsVersion            = "1.1.1"
-  val dynamodbVersion            = "2.16.104"
+  val dynamodbVersion            = "2.17.9"
   val logbackVersion             = "1.2.4"
   val canoeVersion               = "0.5.1"
   val fs2Version                 = "2.5.9"
