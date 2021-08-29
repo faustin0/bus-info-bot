fe1b5a0156780ebd17559f26aee8292b6eb0d93e
Fausto Di Natale <dinatalefausto@gmail.com>
2021-08-29 06:15:04 +0000

    Update testcontainers-scala-dynalite, ... to 0.39.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 //  val http4sVersion              = "0.21.3"
   val catsVersion                = "2.5.3"
   val http4sVersion              = "0.21.26"
-  val testcontainersScalaVersion = "0.39.5"
+  val testcontainersScalaVersion = "0.39.6"
   val kindProjectorV             = "0.13.0"
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.14.1"
