523f8b37e65ca1e27da20982a57f6d2acd017c6e
Fausto Di Natale <dinatalefausto@gmail.com>
2021-10-31 06:16:03 +0000

    Update fs2-core to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val dynamodbVersion            = "2.17.66"
   val logbackVersion             = "1.2.6"
   val canoeVersion               = "0.5.1"
-  val fs2Version                 = "2.5.10"
+  val fs2Version                 = "3.2.2"
 
   lazy val dependencies = Seq(
     "org.typelevel"         %% "cats-effect"         % catsVersion,
