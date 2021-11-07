7df7b95052ab0600d3108d6feb207f9f1b83f451
Fausto Di Natale <dinatalefausto@gmail.com>
2021-11-07 06:17:08 +0000

    Update http4s-blaze-client, http4s-circe, ... to 0.23.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 //  val http4sVersion              = "0.21.3"
   val catsVersion                = "2.5.4"
-  val http4sVersion              = "0.21.31"
+  val http4sVersion              = "0.23.6"
   val testcontainersScalaVersion = "0.39.10"
   val kindProjectorV             = "0.13.2"
   val betterMonadicForV          = "0.3.1"
