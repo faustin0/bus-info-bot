6aaf03951c3d6c72e4c89b87f1da6716d5b8a9b4
Fausto Di Natale <dinatalefausto@gmail.com>
2022-06-26 06:17:38 +0000

    Update log4cats-slf4j to 2.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val kindProjectorV             = "0.13.2"
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.14.2"
-  val log4catsVersion            = "2.3.1"
+  val log4catsVersion            = "2.3.2"
   val dynamodbVersion            = "2.17.214"
   val logbackVersion             = "1.2.11"
   val canoeVersion               = "0.6.0"
