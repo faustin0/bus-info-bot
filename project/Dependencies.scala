4bec6345f9d04aa7fe006de6106303c79663cfc8
Fausto Di Natale <dinatalefausto@gmail.com>
2022-07-31 06:17:52 +0000

    Update fs2-core to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val log4catsVersion            = "2.4.0"
   val logbackVersion             = "1.2.11"
   val canoeVersion               = "0.6.0"
-  val fs2Version                 = "3.2.10"
+  val fs2Version                 = "3.2.11"
   val log4j2Version              = "2.18.0"
 
   lazy val dependencies = Seq(
