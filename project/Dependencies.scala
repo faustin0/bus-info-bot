e8e4d3c3ad3facab587292eaa8ee719a242b30ff
Fausto Di Natale <dinatalefausto@gmail.com>
2022-11-27 06:17:43 +0000

    Revert commit(s) efac7e0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     "org.scalatest"  %% "scalatest"                       % "3.2.13"                   % Test,
     "com.dimafeng"   %% "testcontainers-scala-mockserver" % testcontainersScalaVersion % Test,
     "com.dimafeng"   %% "testcontainers-scala-scalatest"  % testcontainersScalaVersion % Test,
-    "org.mock-server" % "mockserver-client-java"          % "5.14.0"                   % Test,
+    "org.mock-server" % "mockserver-client-java"          % "5.13.2"                   % Test,
     "org.typelevel"  %% "cats-effect-testing-scalatest"   % "1.4.0"                    % Test,
     "org.typelevel"  %% "cats-effect-laws"                % catsVersion                % Test
   )
