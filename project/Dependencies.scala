008f85a703ce2bb99b941821b30cf43b031afafe
Fausto Di Natale <dinatalefausto@gmail.com>
2023-11-19 06:14:26 +0000

    Update http4s-circe, http4s-dsl, ... to 0.23.24

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 //  val http4sVersion              = "0.21.3"
   val catsVersion                = "3.4.11"
-  val http4sVersion              = "0.23.18"
+  val http4sVersion              = "0.23.24"
   val testcontainersScalaVersion = "0.40.17"
   val kindProjectorV             = "0.13.2"
   val betterMonadicForV          = "0.3.1"
