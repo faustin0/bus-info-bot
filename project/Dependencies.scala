f3be0ccdb1908f6f74cfd3ad2ed4d38794196b65
Fausto Di Natale <dinatalefausto@gmail.com>
2024-06-23 06:18:16 +0000

    Update scalatest to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   )
 
   lazy val testDependencies = Seq(
-    "org.scalatest"  %% "scalatest"                       % "3.2.18"                   % Test,
+    "org.scalatest"  %% "scalatest"                       % "3.2.19"                   % Test,
     "com.dimafeng"   %% "testcontainers-scala-mockserver" % testcontainersScalaVersion % Test,
     "com.dimafeng"   %% "testcontainers-scala-scalatest"  % testcontainersScalaVersion % Test,
     "org.mock-server" % "mockserver-client-java"          % "5.13.2"                   % Test,
