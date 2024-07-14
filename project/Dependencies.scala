f172457e31fe31b777bd96e6a79a130e704a233b
Fausto Di Natale <dinatalefausto@gmail.com>
2024-07-14 06:20:10 +0000

    Update circe-generic, circe-literal to 0.14.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val testcontainersScalaVersion = "0.41.4"
   val kindProjectorV             = "0.13.3"
   val betterMonadicForV          = "0.3.1"
-  val circeVersion               = "0.14.8"
+  val circeVersion               = "0.14.9"
   val dynamodbVersion            = "2.26.16"
   val log4catsVersion            = "2.7.0"
   val logbackVersion             = "1.2.11"
