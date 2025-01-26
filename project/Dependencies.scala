6e3947ab1919f32386b2bac198eaa26cfab57cce
Fausto Di Natale <dinatalefausto@gmail.com>
2025-01-26 06:18:47 +0000

    Update testcontainers-scala-mockserver, ... to 0.41.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 //  val http4sVersion              = "0.21.3"
   val catsVersion                = "3.5.7"
   val http4sVersion              = "0.23.30"
-  val testcontainersScalaVersion = "0.41.5"
+  val testcontainersScalaVersion = "0.41.8"
   val kindProjectorV             = "0.13.3"
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.14.10"
